2af284f4e7eb1ed3658140af4da02ae5670f0ac2
utama1995 <redminotee5190@gmail.com>
2026-02-18 14:25:33 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+const API_URL = "https://script.google.com/macros/s/AKfycbzLmDzXPGRK8P6uFeP3UgVrFwidcqlfGCHgloE21CKYYgJPSSOLywLdTF8d6v5MOmOB/exec";
+
+const CLOUDINARY_URL = "https://api.cloudinary.com/v1_1/db00zaan0/image/upload";
+
+const CLOUDINARY_UPLOAD_PRESET = "maintenance_upload";
