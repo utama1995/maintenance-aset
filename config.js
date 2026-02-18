34873b14696d6511676be60383fb1ee318a66015
utama1995 <redminotee5190@gmail.com>
2026-02-18 15:04:16 +0700

    Update API_URL to new endpoint

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbxC4t_WuysKIlVenLWf4ALIqGcuZC8unC1oPEBBDNu1G1mVNpuj66_R87pTcvCCxuruVQ/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbyEH8dkD-u1TU4pHPJ2ONVWWy4cqKoLQlB9jeJxtwdveidn5R5j7OYve7jbIepsFRawxQ/exec";
 
 const CLOUDINARY_URL = "https://api.cloudinary.com/v1_1/db00zaan0/image/upload";
 
