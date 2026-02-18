a219e3661e8ed37cb572e98d007267676d06e777
utama1995 <redminotee5190@gmail.com>
2026-02-18 15:08:30 +0700

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,15 +1,36 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbyEH8dkD-u1TU4pHPJ2ONVWWy4cqKoLQlB9jeJxtwdveidn5R5j7OYve7jbIepsFRawxQ/exec";
+const API_URL =
+"https://script.google.com/macros/s/AKfycbyEH8dkD-u1TU4pHPJ2ONVWWy4cqKoLQlB9jeJxtwdveidn5R5j7OYve7jbIepsFRawxQ/exec";
+
 window.onload = function(){
+
 loadTickets();
+
 };
 
 function loadTickets(){
+
 fetch(API_URL + "?action=getAllTickets")
-.then(res=>res.json())
-.then(data=>{
-const tbody = document.querySelector("#ticketTable tbody");
-tbody.innerHTML="";
-data.reverse().forEach(ticket=>{
+
+.then(res => res.json())
+
+.then(data => {
+
+const tbody =
+document.querySelector("#ticketTable tbody");
+
+tbody.innerHTML = "";
+
+if(!data || data.length === 0){
+
+tbody.innerHTML =
+"<tr><td colspan='5'>Tidak ada data</td></tr>";
+
+return;
+
+}
+
+data.reverse().forEach(ticket => {
+
 tbody.innerHTML += `
 <tr>
 <td>${ticket["Ticket ID"]}</td>
@@ -17,56 +38,24 @@ tbody.innerHTML += `
 <td>${ticket["Nama Aset"]}</td>
 <td>${ticket["Status"]}</td>
 <td>
-<button onclick="editTicket('${ticket["Ticket ID"]}')">
+<button onclick="alert('Edit: ${ticket["Ticket ID"]}')">
 Edit
 </button>
 </td>
 </tr>
 `;
-});
-});
-}
 
-function editTicket(ticket_id){
-fetch(API_URL + "?action=getTicket&ticket_id="+ticket_id)
-.then(res=>res.json())
-.then(ticket=>{
-document.getElementById("modal").style.display="block";
-document.getElementById("ticket_id").value=ticket_id;
-document.getElementById("status").value=ticket["Status"]||"Open";
-document.getElementById("vendor").value=ticket["Vendor"]||"";
-document.getElementById("estimasi").value=formatDate(ticket["Estimasi Selesai"]);
-document.getElementById("catatan").value=ticket["Catatan GA"]||"";
 });
-}
 
-function updateTicket(){
-const data = {
-action:"updateTicket",
-ticket_id:document.getElementById("ticket_id").value,
-status:document.getElementById("status").value,
-vendor:document.getElementById("vendor").value,
-estimasi:document.getElementById("estimasi").value,
-catatan:document.getElementById("catatan").value
-};
-fetch(API_URL,{
-method:"POST",
-body:JSON.stringify(data)
 })
-.then(res=>res.json())
-.then(res=>{
-alert("Updated");
-closeModal();
-loadTickets();
+
+.catch(err => {
+
+console.error(err);
+
+document.querySelector("#ticketTable tbody").innerHTML =
+"<tr><td colspan='5'>Error load data</td></tr>";
+
 });
-}
 
-function closeModal(){
-document.getElementById("modal").style.display="none";
-}
-
-function formatDate(date){
-if(!date) return "";
-const d=new Date(date);
-return d.toISOString().split('T')[0];
 }
